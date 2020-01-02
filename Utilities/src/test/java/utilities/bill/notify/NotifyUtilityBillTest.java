4e97cc120d96781924c4b0fa32c15169fde258a9
Siddharth.S <Siddharth.S@SYNBLRDT552WBN7.synechron.com>
2020-01-02 16:51:49 +0530

    Test Cases

---
+++ Utilities/src/test/java/utilities/bill/notify/NotifyUtilityBillTest.java
@@ -0,0 +1,12 @@
+package utilities.bill.notify;
+
+import org.junit.Test;
+
+public class NotifyUtilityBillTest {
+
+	@Test
+	public void testNotifyBill() throws Exception
+	{
+		NotifyUtilityBill.main(null);
+	}
+}
